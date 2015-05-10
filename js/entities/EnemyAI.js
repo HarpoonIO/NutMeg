d5ae9795b2f5eaf3480919fdfe4e1183564667c6
ThomasHedegaard <thomashj93@gmail.com>
2015-05-10 10:30:58 +0200

    Enemy AI can now rotate and move based on a direction. The is calculated based on human player's coordinates, but there's currently a bug in the claculations... Commit by: Thomas

--- js/entities/EnemyAI.js
+++ js/entities/EnemyAI.js
@@ -4,8 +4,7 @@ var EnemyAI = function (startX, startY, _opponents) {
     var ratio = 0.3;
     var centerX = 300;
     var centerY = 350;
-    var speed = 2;
-    var currentDegrees = 0;
+    var speed = 1;
     var width = 150 * ratio;
     var height = 100 * ratio;
     var opponents = _opponents;
@@ -20,10 +19,22 @@ var EnemyAI = function (startX, startY, _opponents) {
 
     // Compares opponent's placement to its own and updates
     var updateNewCoordinatesWhenMoving = function (movement) {
-        var targetX, targetY;
-        targetX = opponents[0].getX();
-        targetY = opponents[0].getY();
+        var targetX = opponents[0].getX();
+        var targetY = opponents[0].getY();
 
+        var newCenterX = (movement * Math.cos(currentDegrees * Math.PI/180)) + centerX;
+        var newCenterY = (movement * Math.sin(currentDegrees * Math.PI/180)) + centerY;
+        centerX = newCenterX;
+        centerY = newCenterY;
+        coordinates.forEach(function(coord){
+            // VectorX * CurrentX OR VectorY * CurrentY
+            var newX = (movement * Math.cos(currentDegrees * Math.PI/180)) + coord.x;
+            var newY = (movement * Math.sin(currentDegrees * Math.PI/180)) + coord.y;
+            coord.x = newX;
+            coord.y = newY;
+        });
+
+        /*
         console.log("Opponent is at: " + targetX + "," + targetY);
         console.log("AI is at: " + centerX + "," + centerY);
 
@@ -41,6 +52,7 @@ var EnemyAI = function (startX, startY, _opponents) {
 
         // Update coordinates
         coordinates = setCoordinates();
+    */
     };
 
     var updateNewCoordinatesWhenRotating = function (degrees) {
@@ -75,34 +87,58 @@ var EnemyAI = function (startX, startY, _opponents) {
 
     // Finds the opponent's direction and updates
     // AI's if they're different
-    function hasEnemyMoved(){
+    function hasEnemyRotated() {
         var x2 = opponents[0].getX();
         var y2 = opponents[0].getY();
-        var newDirection = 0;
+        var newDirection;
 
         newDirection = Math.round((y2 - centerY) / (x2 - centerX));
 
-        if(newDirection != direction){
-            direction = newDirection;
+        if (newDirection != direction) {
             return true;
-        }else{
+        } else {
             return false;
         }
+    }
 
-
+    function findDirection() {
+        var x2 = opponents[0].getX();
+        var y2 = opponents[0].getY();
+        return Math.round((y2 - centerY) / (x2 - centerX));
     }
 
     function isObstacle() {
         // is there a wall in front?
         // if so, return true
-
         return false;
     }
 
     var update = function () {
-        //updateNewCoordinatesWhenMoving(speed);
+        updateNewCoordinatesWhenMoving(speed);
+        //=========== TANK MOVEMENT LOGIC ============//
+        if(currentDegrees == -rotation){
+            currentDegrees = 360-rotation;
+        }
+        if(currentDegrees == 360){
+            currentDegrees = 0;
+        }
 
-        if (isObstacle() || hasEnemyMoved()) {
+        // Check for player rotation
+        if (hasEnemyRotated()) {
+            var newDirection = findDirection();
+
+            if (newDirection > direction) {
+                currentDegrees += rotation;
+                updateNewCoordinatesWhenRotating(rotation);
+            } else {
+                currentDegrees -= rotation;
+                updateNewCoordinatesWhenRotating(-rotation);
+            }
+            direction = newDirection;
+        }
+
+        // Is there a wall in front of us?
+        if (isObstacle()) {
             updateNewCoordinatesWhenRotating(-rotation);
         }
 
@@ -116,10 +152,10 @@ var EnemyAI = function (startX, startY, _opponents) {
         ctx.shadowColor = "rgb(0, 0, 0)";
         ctx.fillStyle = "#FE2EC8";
         ctx.beginPath();
-        ctx.moveTo(coordinates[0].x,coordinates[0].y);
-        ctx.lineTo(coordinates[1].x,coordinates[1].y);
-        ctx.lineTo(coordinates[2].x,coordinates[2].y);
-        ctx.lineTo(coordinates[3].x,coordinates[3].y);
+        ctx.moveTo(coordinates[0].x, coordinates[0].y);
+        ctx.lineTo(coordinates[1].x, coordinates[1].y);
+        ctx.lineTo(coordinates[2].x, coordinates[2].y);
+        ctx.lineTo(coordinates[3].x, coordinates[3].y);
         ctx.closePath();
         ctx.lineWidth = 4;
         ctx.strokeStyle = '#ff0000';
@@ -131,16 +167,16 @@ var EnemyAI = function (startX, startY, _opponents) {
         ctx.shadowBlur = 0;
         ctx.fillStyle = "#4C0B5F";
         ctx.beginPath();
-        ctx.arc(centerX,centerY,35*ratio,0,2*Math.PI);
+        ctx.arc(centerX, centerY, 35 * ratio, 0, 2 * Math.PI);
         ctx.fill();
         ctx.stroke();
 
         // pipe
         ctx.beginPath();
-        ctx.moveTo(coordinates[4].x,coordinates[4].y);
-        ctx.lineTo(coordinates[5].x,coordinates[5].y);
-        ctx.lineTo(coordinates[6].x,coordinates[6].y);
-        ctx.lineTo(coordinates[7].x,coordinates[7].y);
+        ctx.moveTo(coordinates[4].x, coordinates[4].y);
+        ctx.lineTo(coordinates[5].x, coordinates[5].y);
+        ctx.lineTo(coordinates[6].x, coordinates[6].y);
+        ctx.lineTo(coordinates[7].x, coordinates[7].y);
         ctx.closePath();
         ctx.fill();
 
